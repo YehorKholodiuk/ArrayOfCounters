4e0d48b5e1522dffa6152406e3b631d366d24421
ASUS <Yehor Kholodiuk>
2022-05-01 22:23:15 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
+const initialCounters = [
+  {
+    id:123,
+    value:13
+  },
+  {
+    id:234,
+    value:110
+  },
+  {
+    id:'fdfd4545',
+    value: -80
+  }
+]
 
 function App() {
+  const [counts,setCounts] = useState(initialCounters)
+  const plus = () => {
+    //setCount(count + 1)
+  }
+  const minus = (id) => {
+    console.log(id);
+    const updatedCounts = counts.map( el => el.id === id
+    ? {...el, value:el.value - 1}
+        :el
+    );
+    setCounts(updatedCounts)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+
+        <ul>
+          {counts.map( el=>(
+              <li key={el.id}>
+                <button onClick={() => minus(el.id)}>minus</button>
+                {el.value}
+                <button onClick={plus}>plus</button>
+              </li>
+              )
+
+          )}
+        </ul>
+ <hr/>
+        <button>Add Counter</button>
+      </div>
   );
 }
 
